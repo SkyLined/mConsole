7396e27ae78b27c8c7297b856af768c22a61f825
SkyLined <github@skylined.nl>
2019-08-21 12:21:36 +0200

    Upgrade code that tests imported modules

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,14 +1,50 @@
-import os, sys;
+import json, os, sys;
 
-sModuleFolderPath = os.path.dirname(os.path.abspath(__file__));
-sBaseFolderPath = os.path.dirname(sModuleFolderPath);
-sys.path.extend([
-  sBaseFolderPath,
-  sModuleFolderPath,
-  os.path.join(sModuleFolderPath, "modules"),
-]);
+# Augment the search path to make the test subject a package and have access to its modules folder.
+sTestsFolderPath = os.path.dirname(os.path.abspath(__file__));
+sMainFolderPath = os.path.dirname(sTestsFolderPath);
+sParentFolderPath = os.path.dirname(sMainFolderPath);
+sModulesFolderPath = os.path.join(sMainFolderPath, "modules");
+asOriginalSysPath = sys.path[:];
+sys.path = [sParentFolderPath, sModulesFolderPath] + asOriginalSysPath;
+# Load product details
+oProductDetailsFile = open(os.path.join(sMainFolderPath, "dxProductDetails.json"), "rb");
+try:
+  dxProductDetails = json.load(oProductDetailsFile);
+finally:
+  oProductDetailsFile.close();
+# Save the list of names of loaded modules:
+asOriginalModuleNames = sys.modules.keys();
 
+__import__(dxProductDetails["sProductName"], globals(), locals(), [], -1);
+
+# Sub-packages should load all modules relative, or they will end up in the global namespace, which means they may get
+# loaded by the script importing it if it tries to load a differnt module with the same name. Obviously, that script
+# will probably not function when the wrong module is loaded, so we need to check that we did this correctly.
+asUnexpectedModules = list(set([
+  sModuleName.lstrip("_").split(".", 1)[0] for sModuleName in sys.modules.keys()
+  if not (
+    sModuleName in asOriginalModuleNames # This was loaded before
+    or sModuleName.lstrip("_").split(".", 1)[0] in (
+      [dxProductDetails["sProductName"]] +
+      dxProductDetails["asDependentOnProductNames"] +
+      [
+        # These built-in modules are expected:
+        "collections", "ctypes", "gc", "heapq", "itertools", "keyword", "msvcrt",
+        "platform", "string", "strop", "struct", "subprocess", "thread",
+        "threading", "time",
+      ]
+    )
+  )
+]));
+assert len(asUnexpectedModules) == 0, \
+      "Module(s) %s was/were unexpectedly loaded!" % ", ".join(sorted(asUnexpectedModules));
+
+#Import the test subject
 from oConsole import oConsole;
+
+# Restore the search path
+sys.path = asOriginalSysPath;
 
 for uBackground in xrange(0, 0x10):
   asLineOutput = [];
